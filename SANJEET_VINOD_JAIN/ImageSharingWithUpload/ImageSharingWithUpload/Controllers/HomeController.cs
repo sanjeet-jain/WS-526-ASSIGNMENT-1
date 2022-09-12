a3f3d5c08fe37bf74766f35b28cb483c72137c8c
sanjeet-jain <sanjeetjain241210@gmail.com>
2022-09-11 21:48:45 -0400

    added privacy page

--- SANJEET_VINOD_JAIN/ImageSharingWithUpload/ImageSharingWithUpload/Controllers/HomeController.cs
+++ SANJEET_VINOD_JAIN/ImageSharingWithUpload/ImageSharingWithUpload/Controllers/HomeController.cs
@@ -1,46 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Diagnostics;
+using ImageSharingWithUpload.Models;
 using Microsoft.AspNetCore.Mvc;
 
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
+namespace ImageSharingWithUpload.Controllers;
 
-using ImageSharingWithUpload.Models;
-
-namespace ImageSharingWithUpload.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    protected void CheckAda()
     {
-        protected void CheckAda()
-        {
-            var cookie = Request.Cookies["ADA"];
-            if (cookie != null && "true".Equals(cookie))
-            {
-                ViewBag.isADA = true;
-            }
-            else
-            {
-                ViewBag.isADA = false;
-            }
-        }
+        var cookie = Request.Cookies["ADA"];
+        if (cookie != null && "true".Equals(cookie))
+            ViewBag.isADA = true;
+        else
+            ViewBag.isADA = false;
+    }
 
-        [HttpGet]
-        public IActionResult Index(String id = "Stranger")
-        {
-            CheckAda();
-            ViewBag.Title = "Welcome!";
-            ViewBag.Id = id;
-            return View();
-        }
+    [HttpGet]
+    public IActionResult Index(string id = "Stranger")
+    {
+        CheckAda();
+        ViewBag.Title = "Welcome!";
+        ViewBag.Id = id;
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet]
+    public IActionResult Privacy()
+    {
+        CheckAda();
+        return View();
     }
 }
